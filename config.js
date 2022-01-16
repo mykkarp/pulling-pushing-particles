c44a93f743b36ce2841e3c32fb083de1ded845ae
mykkarp <mykkarp@gmail.com>
2022-01-16 16:00:28 +0300

    pulling pushing

--- config.js
+++ config.js
@@ -2,6 +2,10 @@ export const config = {
   dotMinRadius: 6,
   dotMaxRadius: 20,
   massFactor: 0.002,
-  defFillColor: `rgba(250, 10, 30, 0.4)`,
-  defStrokeColor: `rgba(250, 10, 30, 0.8)`
+  defFillColor: `rgba(250, 10, 30, 0.8)`,
+  defStrokeColor: `rgba(250, 10, 30, 1)`,
+  smooth: 0.75,
+  sphereRad: 300,
+  bigDotRad: 35,
+  mouseSize: 120
 }
--- script.js
+++ script.js
@@ -10,16 +10,18 @@ const mouse = { x: w / 2, y: h / 2, isDown: false };
 const dots = [];
 
 class Dot {
-  constructor() {
+  constructor(bigDotRad) {
     this.pos = { x: mouse.x, y: mouse.y };
-    this.rad = random(config.dotMinRadius, config.dotMaxRadius);
+    this.rad = bigDotRad || random(config.dotMinRadius, config.dotMaxRadius);
     this.fill = config.defFillColor;
     this.stroke = config.defStrokeColor;
     this.vel = { x: 0, y: 0 };
     this.mass = this.rad * config.massFactor;
   }
 
-  draw() {
+  draw(mouseX, mouseY) {
+    this.pos.x = mouseX ? mouseX : this.pos.x + this.vel.x;
+    this.pos.y = mouseY ? mouseY : this.pos.y + this.vel.y;
     const { x, y } = this.pos;
     ctx.fillStyle = this.fill;
     ctx.strokeStyle = this.stroke;
@@ -31,6 +33,7 @@ class Dot {
   }
 }
 
+dots.push(new Dot(config.bigDotRad));
 loop();
 
 canvas.addEventListener('mousemove', setMousePosition);
@@ -41,9 +44,33 @@ function loop() {
   ctx.clearRect(0, 0, w, h);
 
   if (mouse.isDown) dots.push(new Dot());
-  dots.forEach(dot => dot.draw());
+  updateDots();
 
   requestAnimationFrame(loop);
+}
+function updateDots() {
+  for (let i = 1; i < dots.length; i++) {
+    const acceleration = { x: 0, y: 0 };
+    for (let j = 0; j < dots.length; j++) {
+      const [a, b] = [dots[i], dots[j]];
+
+      const delta = { x: b.pos.x - a.pos.x, y: b.pos.y - a.pos.y }
+      const distance = Math.sqrt(delta.x ** 2 + delta.y ** 2) || 1;
+      let pullingForce = (distance - config.sphereRad) / distance * b.mass;
+
+      if (j === 0) {
+        const alpha = config.mouseSize / distance;
+        a.fill = `rgba(250, 10, 30, ${alpha})`;
+        distance < config.mouseSize ? pullingForce = (distance - config.mouseSize) * b.mass : pullingForce = b.mass;
+      }
+
+      acceleration.x += delta.x * pullingForce;
+      acceleration.y += delta.y * pullingForce;
+    }
+    dots[i].vel.x = dots[i].vel.x * config.smooth + acceleration.x * dots[i].mass;
+    dots[i].vel.y = dots[i].vel.y * config.smooth + acceleration.y * dots[i].mass;
+  }
+  dots.forEach((dot, i) => i === 0 ? dot.draw(mouse.x, mouse.y) : dot.draw());
 }
 function random(min, max) {
   return Math.random() * (max - min) + min;
